3576f63c84ae1c67b4c0c47f79531cc6858b860b
Tima Vlasto <koheli@hotmail.com>
2025-05-22 13:35:49 +0300

    Create wp-debug-config.php
    Debugging without exposing errors to users

---
+++ wp-debug-config.php
@@ -0,0 +1,6 @@
+<?php
+// Add to wp-config.php to enable WP_DEBUG
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true); // Logs to wp-content/debug.log
+define('WP_DEBUG_DISPLAY', false); // Hide errors on screen
+@ini_set('display_errors', 0);
